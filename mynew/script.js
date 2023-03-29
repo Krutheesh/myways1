74c81baa4a763b2eb5b5ccfc43f733d6f63a35f4
Krutheesh <119755806+Krutheesh@users.noreply.github.com>
2023-03-29 17:57:28 +0530

    Add files via upload

---
+++ mynew/script.js
@@ -0,0 +1,11 @@
+const calculateAnswer = (email) =>
+
+{
+
+const [localPart, domain] = email.split("@");
+
+const [hostname, ...countryCodes] = domain.split(".");
+
+return `${localPart.length}${hostname.length}${countryCodes.reduce( (a,cc) => a + cc.length, "") }` }
+
+console.log(calculateAnswer("krutheeshsiripuram@gmail.com"))
